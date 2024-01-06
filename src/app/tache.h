559f57719883d50cfcb4b728486d0c8411f168f9
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2024-01-06 13:48:45 +0100

    Doxygen
    Also added the documentation on the classes

--- src/app/tache.h
+++ src/app/tache.h
@@ -8,36 +8,65 @@
 
 typedef enum {false, true} bool;
 
-typedef struct adresse{
-    int numero;
-    char nomRue[MAX_LIGNE];
-    char ville[MAX_LIGNE];
-    int codePostal;
+/**
+ * @struct Adresse
+ * @brief Structure représentant une adresse.
+ *
+ * Cette structure contient les informations nécessaires pour représenter une adresse,
+ * y compris le numéro de rue, le nom de la rue, la ville et le code postal.
+ */
+typedef struct adresse {
+    int numero; /**< Le numéro de la rue. */
+    char nomRue[MAX_LIGNE]; /**< Le nom de la rue. */
+    char ville[MAX_LIGNE]; /**< Le nom de la ville. */
+    int codePostal; /**< Le code postal de la ville. */
 } Adresse;
 
-// Structure pour un devis
+/**
+ * @struct Devis
+ * @brief Structure représentant un devis.
+ *
+ * Cette structure contient les détails spécifiques d'un devis,
+ * tels que le nom de la tâche, l'entreprise concernée, l'adresse, le capital, la durée et le coût.
+ */
 typedef struct Devis {
-    char nomTache[MAX_TRAVAUX];
-    char entreprise[MAX_TRAVAUX];
-    Adresse adresse;
-    int capital;
-    int duree;
-    int cout;
+    char nomTache[MAX_TRAVAUX]; /**< Le nom de la tâche du devis. */
+    char entreprise[MAX_TRAVAUX]; /**< Le nom de l'entreprise concernée par le devis. */
+    Adresse adresse; /**< L'adresse de l'entreprise. */
+    int capital; /**< Le capital nécessaire pour la tâche. */
+    int duree; /**< La durée estimée de la tâche. */
+    int cout; /**< Le coût estimé de la tâche. */
 } Devis;
 
-// Structure pour les maillons de la liste de devis
+/**
+ * @struct MaillonDevis
+ * @brief Structure représentant un maillon d'une liste de devis.
+ *
+ * Cette structure contient un devis et un pointeur vers le maillon suivant dans la liste.
+ */
 typedef struct MaillonDevis {
-    Devis devis;
-    struct MaillonDevis* suivant;
+    Devis devis; /**< Le devis contenu dans le maillon. */
+    struct MaillonDevis* suivant; /**< Le pointeur vers le maillon suivant dans la liste. */
 } MaillonDevis;
 
-// Structure pour la liste de devis
+/**
+ * @typedef ListeDevis
+ * @brief Type de pointeur vers un MaillonDevis.
+ *
+ * Ce type représente une liste de devis, qui est essentiellement une liste chaînée de MaillonDevis.
+ */
 typedef struct MaillonDevis* ListeDevis;
 
-// Structure pour une offre de travaux
+/**
+ * @struct Offre
+ * @brief Structure représentant une offre de travaux.
+ *
+ * Cette structure contient les détails d'une offre de travaux, y compris le type de travaux
+ * et une liste de devis associée.
+ */
 typedef struct Offre {
-    char travaux[MAX_TRAVAUX];
-    ListeDevis ldevis;
+    char travaux[MAX_TRAVAUX]; /**< Le type de travaux offerts. */
+    ListeDevis ldevis; /**< La liste de devis associée à l'offre. */
 } Offre;
 
 /**
